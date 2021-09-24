40d97e3670810d2637ff70eba2943efeba9c0dc2
Warwick Stone <warwick_stone@fastmail.com>
2021-09-24 21:40:42 +1000

    WIP: init infection pile

--- pandemic.test/GameSetupTests.cs
+++ pandemic.test/GameSetupTests.cs
@@ -16,10 +16,16 @@ namespace pandemic.test
             eventLog.AddRange(PandemicGame.SetDifficulty(eventLog, Difficulty.Normal));
             eventLog.AddRange(PandemicGame.SetInfectionRate(eventLog, 2));
             eventLog.AddRange(PandemicGame.SetOutbreakCounter(eventLog, 0));
+            // eventLog.AddRange(PandemicGame.SetupInfectionDeck(eventLog));
             eventLog.AddRange(PandemicGame.AddPlayer(eventLog, Role.Medic));
 
             var state = PandemicGame.FromEvents(eventLog);
             Assert.AreEqual(Difficulty.Normal, state.Difficulty);
+            Assert.AreEqual(2, state.InfectionRate);
+            Assert.AreEqual(0, state.OutbreakCounter);
+            Assert.AreEqual(1, state.Players.Count);
+            Assert.AreEqual(48, state.InfectionDrawPile.Count);
+            Assert.AreEqual(0, state.InfectionDiscardPile.Count);
         }
     }
 }
