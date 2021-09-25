cd4294b65f4932344f4332b50599896b0d6c7418
Warwick Stone <warwick_stone@fastmail.com>
2021-09-25 22:30:06 +1000

    start using record shortcuts

--- pandemic/Events/InfectionDeckSetUp.cs
+++ pandemic/Events/InfectionDeckSetUp.cs
@@ -1,16 +1,16 @@
 using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Immutable;
 using pandemic.Values;
 
 namespace pandemic.Events
 {
     public record InfectionDeckSetUp : IEvent
     {
-        public List<InfectionCard> Deck { get; }
+        public ImmutableList<InfectionCard> Deck { get; }
 
         public InfectionDeckSetUp(IEnumerable<InfectionCard> deck)
         {
-            Deck = deck.ToList();
+            Deck = deck.ToImmutableList();
         }
     }
 }
