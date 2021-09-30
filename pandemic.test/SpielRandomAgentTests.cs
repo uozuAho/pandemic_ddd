6549a334bba7df4403a1ac263876625149998a37
Warwick Stone <warwick_stone@fastmail.com>
2021-09-30 22:43:37 +1000

    random actions

--- pandemic.test/SpielRandomAgentTests.cs
+++ pandemic.test/SpielRandomAgentTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using pandemic.Aggregates;
 
@@ -8,14 +11,23 @@ namespace pandemic.test
         [Test]
         public void asdf()
         {
+            var random = new Random();
             var state = new PandemicSpielGameState(PandemicGame.CreateUninitialisedGame());
 
             for (var i = 0; i < 1000 && !state.IsTerminal; i++)
             {
-                state.ApplyAction(0);
+                var action = Choice(state.LegalActions(), random);
+                state.ApplyAction(action);
             }
 
             Assert.IsTrue(state.IsTerminal, "Expected to reach terminal state in under 1000 actions");
+        }
+
+        private static T Choice<T>(IEnumerable<T> items, Random random)
+        {
+            var itemList = items.ToList();
+            var idx = random.Next(0, itemList.Count);
+            return itemList[idx];
         }
     }
 }
