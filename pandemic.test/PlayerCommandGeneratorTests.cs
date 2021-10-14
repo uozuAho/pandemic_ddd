52ffe5e8b3d57c778aa2e4045ad2b8ef6ba27290
Warwick Stone <warwick_stone@fastmail.com>
2021-10-14 22:00:08 +1100

    test fail: bad move generator!

---
+++ pandemic.test/PlayerCommandGeneratorTests.cs
@@ -0,0 +1,32 @@
+using System.Collections.Immutable;
+using System.Linq;
+using NUnit.Framework;
+using pandemic.Aggregates;
+using pandemic.GameData;
+using pandemic.Values;
+
+namespace pandemic.test
+{
+    class PlayerCommandGeneratorTests
+    {
+        [Test]
+        public void Cannot_build_research_station_when_one_already_exists()
+        {
+            var generator = new PlayerCommandGenerator();
+            var atlantaPlayerCard = new PlayerCityCard(new CityData {Name = "Atlanta"});
+
+            var game = PandemicGame.CreateUninitialisedGame() with
+            {
+                Players = ImmutableList.Create(new Player
+                {
+                    // atlanta starts with a research station
+                    Location = "Atlanta",
+                    Hand = new PlayerHand(new[] {atlantaPlayerCard})
+                })
+            };
+
+            // todo: rename LegalMoves to legal commands
+            Assert.False(generator.LegalMoves(game).Any(c => c is BuildResearchStationCommand));
+        }
+    }
+}
