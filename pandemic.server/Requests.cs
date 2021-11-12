d4cdac8e8ca05e118f452604f9e1c1ff94140999
Warwick Stone <uozu.aho@gmail.com>
2021-11-12 18:21:35 +1100

    rename stuff

--- pandemic.server/Requests.cs
+++ pandemic.server/Requests.cs
@@ -13,14 +13,4 @@ namespace pandemic.server
             this.state_str = stateStr;
         }
     }
-
-    public record StateResponse(
-        string current_player,
-        int[] legal_actions,
-        bool is_terminal,
-        bool is_chance_node,
-        double[] returns,
-        string state_str,
-        string pretty_str
-    );
 }
