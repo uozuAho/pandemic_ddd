299d3b94bae359f243c8f521e11498e661e449a8
Warwick Stone <warwick_stone@fastmail.com>
2021-11-20 16:46:05 +1100

    server: parameter_specification is dict

--- pandemic.server/ZmqGameServer.cs
+++ pandemic.server/ZmqGameServer.cs
@@ -83,7 +83,7 @@ namespace pandemic.server
                 provides_information_state_tensor = false,
                 provides_observation_string = false,
                 provides_observation_tensor = false,
-                parameter_specification = Array.Empty<int>()
+                parameter_specification = new Dictionary<string, string>()
             };
         }
 
