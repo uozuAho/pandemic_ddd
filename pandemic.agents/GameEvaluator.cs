6af3911fd2b63099afe93fb1237986f60bcec68f
Warwick Stone <warwick_stone@fastmail.com>
2021-12-21 20:31:48 +1100

    start a game evaluator

---
+++ pandemic.agents/GameEvaluator.cs
@@ -0,0 +1,39 @@
+using System.Linq;
+using pandemic.Aggregates;
+
+namespace pandemic.agents
+{
+    internal class GameEvaluator
+    {
+        /// <summary>
+        /// Return a value that evaluates how 'good' a state is, ie.
+        /// how likely a win is from this state. Higher values are
+        /// better.
+        /// </summary>
+        public int Evaluate(PandemicGame game)
+        {
+            if (game.IsWon) return int.MaxValue;
+            if (game.IsLost) return int.MinValue;
+            // todo: impossible to win = min value
+
+            var score = 0;
+
+            // diseases cured is great
+            score += game.CureDiscovered.Sum(c => c.Value ? 1000 : 0);
+
+            // each research station on a unique color is good
+            score += game.Cities
+                .Where(c => c.HasResearchStation)
+                .GroupBy(c => game.Board.City(c.Name).Colour)
+                .Sum(_ => 100);
+
+            // todo: cards of same colour in hand
+
+            // bad stuff -----------------------
+            // outbreaks are bad
+            score -= game.OutbreakCounter * 100;
+
+            return 0;
+        }
+    }
+}
