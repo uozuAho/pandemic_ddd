cd089fbcaf9c7262ec0dfc7be0aae9a7f0f113c1
Warwick Stone <warwick_stone@fastmail.com>
2022-01-16 14:22:53 +1100

    add skeleton of agent comparer

---
+++ pandemic.console/AgentComparer.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using pandemic.Aggregates;
+using pandemic.Values;
+
+namespace pandemic.console
+{
+    internal class AgentComparer
+    {
+        public static void Run()
+        {
+            foreach (var agent in agents)
+            {
+                var stats = RunAgent(agent);
+            }
+        }
+
+        private static AgentStats RunAgent(PandemicAgent agent)
+        {
+            var sw = Stopwatch.StartNew();
+            var stats = new AgentStats();
+            while (sw.Elapsed < TimeSpan.FromSeconds(60) && stats.GamesPlayed < 20)
+            {
+                var game = NewGame();
+                var result = FindWin(game, agent, TimeSpan.FromSeconds(2));
+                stats.GamesPlayed++;
+            }
+        }
+
+        private static AgentRunResult FindWin(
+            PandemicGame game,
+            PandemicAgent agent,
+            TimeSpan timeLimit)
+        {
+            var sw = Stopwatch.StartNew();
+            while (sw.Elapsed < timeLimit)
+            {
+                var command = agent.GetCommand(game);
+                game = game.Do(command);
+                if (game.IsWon)
+                    return AgentRunResult.FoundWin;
+            }
+
+            return AgentRunResult.Timeout;
+        }
+
+        private static PandemicGame NewGame()
+        {
+            var (game, events) = PandemicGame.CreateNewGame(new NewGameOptions
+            {
+                Difficulty = Difficulty.Introductory,
+                // todo: try more players
+                Roles = new[] { Role.Medic, Role.QuarantineSpecialist }
+            });
+
+            return game;
+        }
+    }
+
+    internal record AgentStats
+    {
+        public int GamesPlayed { get; set; }
+        public int NumWins { get; set; }
+        public int NumTimeouts { get; set; }
+
+        private List<TimeSpan> GameTimes = new();
+        private List<TimeSpan> WinTimes = new();
+    }
+
+    enum AgentRunResult
+    {
+        Timeout,
+        FoundWin
+    }
+}
