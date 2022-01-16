f80ad07b1a1cf9cd58e3f12d399694535643e24a
Warwick Stone <warwick_stone@fastmail.com>
2022-01-16 16:03:07 +1100

    use PandemicGame.Do in zmq server

--- pandemic.server/ZmqGameServer.cs
+++ pandemic.server/ZmqGameServer.cs
@@ -161,23 +161,8 @@ namespace pandemic.server
 
         public PandemicGame ApplyAction(PandemicGame game, PlayerCommand action)
         {
-            switch (action)
-            {
-                case DriveFerryCommand command:
-                    (game, _) = game.DriveOrFerryPlayer(command.Role, command.City);
-                    return game;
-                case DiscardPlayerCardCommand command:
-                    (game, _) = game.DiscardPlayerCard(command.Card);
-                    return game;
-                case BuildResearchStationCommand command:
-                    (game, _) = game.BuildResearchStation(command.City);
-                    return game;
-                case DiscoverCureCommand command:
-                    (game, _) = game.DiscoverCure(command.Cards);
-                    return game;
-                default:
-                    throw new ArgumentOutOfRangeException($"Unsupported action: {action}");
-            }
+            (game, _) = game.Do(action);
+            return game;
         }
     }
 }
