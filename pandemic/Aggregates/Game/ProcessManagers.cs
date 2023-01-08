7b78d07d97eeb33adbfdb3755766c06cd467b46b
Warwick Stone <warwick_stone@fastmail.com>
2023-01-08 17:44:00 +1100

    asdf

--- pandemic/Aggregates/Game/ProcessManagers.cs
+++ pandemic/Aggregates/Game/ProcessManagers.cs
@@ -11,14 +11,11 @@ public partial record PandemicGame
     {
         ThrowIfGameOver(game);
 
-        if (game.PlayerDrawPile.Count == 0)
-            return game.ApplyEvent(new GameLost("No more player cards"), events);
-
         if (game.PhaseOfTurn == TurnPhase.DrawCards) game = DrawCards(game, events);
 
         if (game.IsOver) return game;
 
-        if (game.CurrentPlayer.Hand.Count > 7) return game;
+        if (game.APlayerMustDiscard) return game;
 
         if (game.PhaseOfTurn == TurnPhase.InfectCities) game = InfectCities(game, events);
 
@@ -37,6 +34,9 @@ public partial record PandemicGame
 
     private static PandemicGame DrawCards(PandemicGame game, ICollection<IEvent> events)
     {
+        if (game.PlayerDrawPile.Count == 0)
+            return game.ApplyEvent(new GameLost("No more player cards"), events);
+
         game = PickUpCard(game, events);
 
         if (events.Last() is EpidemicTriggered) game = Epidemic(game, events);
