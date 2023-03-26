26f8fd3bb4e308cab862793665f18b1e4e3ac91f
Warwick Stone <warwick_stone@fastmail.com>
2023-03-26 16:18:54 +1100

    add random and greedy runner

---
+++ pandemic.perftest/GameRunner.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using pandemic.agents;
+using pandemic.Aggregates.Game;
+using pandemic.Commands;
+using pandemic.test.Utils;
+using pandemic.Values;
+using utils;
+
+namespace pandemic.perftest;
+
+public static class GameRunner
+{
+    public static RunStats RunRandomGames(TimeSpan totalRunTime)
+    {
+        var agent = new RandomAgent();
+        return RunGames(agent, totalRunTime);
+    }
+
+    public static RunStats RunGreedyGames(TimeSpan totalRunTime)
+    {
+        var agent = new GreedyAgent();
+        return RunGames(agent, totalRunTime);
+    }
+
+    private static RunStats RunGames(ILiveAgent agent, TimeSpan totalRunTime)
+    {
+        var totalTimer = Stopwatch.StartNew();
+        var numGames = 0;
+        var commandsExecuted = 0;
+
+        while (totalTimer.Elapsed < totalRunTime)
+        {
+            var game = NewGame();
+
+            while (!game.IsOver && totalTimer.Elapsed < totalRunTime)
+            {
+                var command = agent.NextCommand(game);
+                (game, _) = game.Do(command);
+                commandsExecuted++;
+            }
+            numGames++;
+        }
+
+        return new RunStats(numGames, commandsExecuted);
+    }
+
+    private static PandemicGame NewGame()
+    {
+        var options = NewGameOptionsGenerator.RandomOptions() with
+        {
+            Roles = new[] { Role.Medic, Role.Dispatcher },
+            Difficulty = Difficulty.Introductory,
+            CommandGenerator = new SensibleCommandGenerator()
+        };
+
+        var (game, _) = PandemicGame.CreateNewGame(options);
+
+        return game with { SelfConsistencyCheckingEnabled = false };
+    }
+}
---
+++ pandemic.perftest/Program.cs
@@ -0,0 +1,9 @@
+﻿using pandemic.perftest;
+
+Console.WriteLine("Running random games...");
+var stats = GameRunner.RunRandomGames(TimeSpan.FromSeconds(2));
+Console.WriteLine($"Games played: {stats.GamesPlayed}, commands executed: {stats.CommandsExecuted}");
+
+Console.WriteLine("Running greedy games...");
+stats = GameRunner.RunGreedyGames(TimeSpan.FromSeconds(2));
+Console.WriteLine($"Games played: {stats.GamesPlayed}, commands executed: {stats.CommandsExecuted}");
